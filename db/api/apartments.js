03323c5c8bf9159cf4b6df5d6d1aa711b83c196f
Aviv2991 <55682481+Aviv2991@users.noreply.github.com>
2020-01-27 13:43:22 +0200

    added wish list and pagenation

--- db/api/apartments.js
+++ db/api/apartments.js
@@ -1,6 +1,6 @@
 const connection = require('../config');
 
-function getAll({id,maxNumberOfBaths,minNumberOfBaths,maxNumberOfRooms,minNumberOfRooms,propertyType, city,minSqft,maxSqft, country,minPrice,maxPrice, page = 1, size = 100}) {
+function getAll({id,maxNumberOfBaths,minNumberOfBaths,maxNumberOfRooms,minNumberOfRooms,propertyType, city,minSqft,maxSqft, country,minPrice,maxPrice, page = 1, size = 20}) {
     return new Promise((resolve, reject) => {
         const params = [];
         const query = `Select ap.*   from apartments ap 
@@ -12,12 +12,12 @@ function getAll({id,maxNumberOfBaths,minNumberOfBaths,maxNumberOfRooms,minNumber
          and 
          (${!country ? '1' : (params.push(country), ' cn.name = ? ')})
          and
-         (${!minPrice ? '1' : (params.push(minPrice),'ap.price >= ?')})
+         (${!minPrice ? '1' : (params.push(minPrice),'ap.price >= ?')}) 
+         and 
+         (${!maxPrice ? '1' : (params.push(maxPrice),'ap.price <= ?')}) 
          and
-         (${!maxPrice ? '1' : (params.push(maxPrice),'ap.price <= ?')})
-         and
-         (${!maxNumberOfRooms ? '1' : (params.push(maxNumberOfRooms),'ap.number_of_room <= ?')})
-         and
+         (${!maxNumberOfRooms ? '1' : (params.push(maxNumberOfRooms),'ap.number_of_room <= ?')}) 
+         and 
          (${!minNumberOfRooms ? '1' : (params.push(minNumberOfRooms),'ap.number_of_room >= ?')})
          and
          (${!propertyType ? '1' : (params.push(propertyType),'ap.property_type = ?')})
@@ -65,24 +65,53 @@ function getImagesById(apartmentId){
         });
     }); 
 }
-function addApartment({id, user_id, address,city_id, price, number_of_room,number_of_bath ,sqft, created_on,description, sale_status, availability,property_type, main_image}) {
+function addApartment({user_id, address,city_id, price, number_of_room,number_of_bath ,sqft, description, sale_status, availability,property_type, main_image}) {
     const apartmentPromise = new Promise((resolve,reject)=>{
         const query = 
-            'insert into apartments (id, user_id, address,city_id, price, number_of_room,number_of_bath ,sqft, created_on,description, sale_status, availability,property_type, main_image) values(default,?,?,?,?,?,?,?,?,?,?,?,?,?)';
-        const params = [id, user_id, address,city_id, price, number_of_room,number_of_bath ,sqft, created_on,description, sale_status, availability,property_type, main_image];
+            'insert into apartments (user_id, address, city_id, price, number_of_room, number_of_bath , sqft, description, sale_status, availability, property_type, main_image) values(?,?,?,?,?,?,?,?,?,?,?,?)';
+            const params = [user_id, address,city_id, price, number_of_room,number_of_bath ,sqft, description, sale_status, availability,property_type, main_image];
         connection.query(query,params,(error,result,fields)=>{
-            if(error) reject(error);
-            resolve(result);
+            console.log('result:', result);
+            if(error) reject(error); 
+            resolve(result.insertId);
         });     
-    });
-    return apartmentPromise;
+    });  
+    return apartmentPromise;  
+}
+function addImagesToApartment(apartment_id,imagesArr) {
+    return new Promise((resolve,reject)=>{
+        let data = '';
+        imagesArr.map(image => data += (`(${apartment_id},${" '" +image.destination+image.filename+ " ' "}),`));
+        data = data.slice(0,data.length-1);
+        console.log(data)
+        connection.query(`insert into images (apartment_id,url) values ${data}`,(error,results,fields)=>{
+            if(error)reject(error);
+            resolve(results); 
+        })
+    })
+};
+
+function getApartmentsByUserId(userId) {
+    console.log(userId);
+    return new Promise((resolve,reject) => {
+        connection.query(`SELECT * FROM realtor.apartments where user_id=?`,[userId],(error,results,fields)=>{
+            console.log('im in the auery')
+            if(error){
+                reject(error);
+                return;
+            }
+            resolve(results);
+        })
+    })
 }
 
                     
 module.exports = {
     getAll,
-    byId, 
+    byId,  
     getImagesById,
-    addApartment
+    addApartment,
+    addImagesToApartment,
+    getApartmentsByUserId,
 };
 
--- routes/countries.js
+++ routes/countries.js
@@ -1,20 +1,20 @@
 const express = require('express');
 var router = express.Router();
-const { getCitiesByCountryId,getAll , byId } = require('../db/api/countries.js');
+const { getCitiesByCountryName,getAll , byId } = require('../db/api/countries.js');
 router.get('/',function(req,res,next){
     getAll()
-        .then(countries=>{res.status(200).json({countries});console.log(countries)})
+        .then(countries=> res.status(200).json({countries}))
         .catch(error=>res.status(500).json({error:error.message}));
 });
 
-// router.get('/:countryId',function(req,res,next){
-//     byId(req.params.countryId)
-//     .then(countries=>res.status(200).json(countries))
-//     .catch(error=>res.status(500).json({error:error.message}));
-// });
+router.get('/:countryId',function(req,res,next){
+    byId(req.params.countryId)
+    .then(countries=>res.status(200).json(countries))
+    .catch(error=>res.status(500).json({error:error.message}));
+});
 
 router.get('/:countryName/cities',function(req,res,next){
-    getCitiesByCountryId(req.params.countryName)
+    getCitiesByCountryName(req.params.countryName)
     .then(cities=>{res.status(200).json(cities);console.log(cities)})
     .catch(error=>res.status(500).json({error:error.message}));
 }); 
